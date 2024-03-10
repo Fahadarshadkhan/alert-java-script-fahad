97d6bdb16e1cd3730d49de35760e4003e811acfd
Fahadarshadkhan <154579872+Fahadarshadkhan@users.noreply.github.com>
2024-03-10 15:33:09 +0500

    Add files via upload
    alert java-script-fahad

---
+++ app.js
@@ -0,0 +1,9 @@
+
+alert("hello world")
+alert("please enter a valid passward")
+alert("welcome to js land happy coding")
+alert("welcome to java script")
+alert("please enter your age")
+alert("please enter your gender")
+let a = prompt("enter a here")
+document.write(a)
